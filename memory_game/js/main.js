d196c113fd88eabcaced2aa0d0ef899f0b9cb755
Raul Gutierrez <raulgutierrez@silver-owl.attlocal.net>
2017-09-09 15:40:53 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,32 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds"
+	}
+
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-	
+
 		if(cardsInPlay.length === 2){
 			if(cardsInPlay[0] === cardsInPlay[1]){
 				alert("You found a match!");
@@ -16,9 +39,11 @@ var checkForMatch = function(){
 
 var flipCard = function(cardID){
 
-	console.log("User flipped " + cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 
-	cardsInPlay.push(cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
 
 	checkForMatch();
 
