070ade452aa22d722ea4edb6b5f40c7c9df7812e
Raul Gutierrez <raulgutierrez@silver-owl.attlocal.net>
2017-09-09 15:03:24 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,23 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped" + cardThree);
-console.log("User flipped" + cardOne);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again!");
+	}
+};
+
+
