58ac54c7d87bdfc74e5a5b6b0f2f5cac45adcea0
Raul Gutierrez <raulgutierrez@silver-owl.attlocal.net>
2017-09-10 11:41:31 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,36 +24,77 @@ var cards = [
 ];
 
 var cardsInPlay = [];
+var suitsInPlay = []
 
-var checkForMatch = function() {
 
-		if(cardsInPlay.length === 2){
-			if(cardsInPlay[0] === cardsInPlay[1]){
-				alert("You found a match!");
-				
-				resetCards();
-			}else{
-				alert("Sorry, try again!");
-		}
-	};
-
-};
 
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	console.log(cardId);
 
 	cardsInPlay.push(cards[cardId].rank);
+	suitsInPlay.push(cards[cardId].suit);
+
 	this.setAttribute('src', cards[cardId].cardImage);
+
+	var finalImage = cards[cardId].cardImage;
+
+	var resetCard = function() {
+		finalImage = 'images/back.png';
+		cardsInPlay.pop();
+		suitsInPlay.pop();
+	};
+
+	
+	var checkForMatch = function() {
+
+		if(cardsInPlay.length === 2) {
+			if(suitsInPlay[0] === suitsInPlay[1] && cardsInPlay[0] === cardsInPlay[1]){
+				cardsInPlay.pop();
+				suitsInPlay.pop();
+				resetCard();
+				return;
+			}
+
+			if(cardsInPlay[0] === cardsInPlay[1]){
+				alert("You found a match!");
+				
+			}else{
+				alert("Sorry, try again!");
+				resetCard();
+			}
+		
+		};
+
+		if(cardsInPlay.length === 4){
+			if(cardsInPlay[2] === cardsInPlay[3]){
+				alert("You found a match!");
+				
+			}else{
+				alert("Sorry, try again!");
+				resetCard();
+			}
+		};
+
+	};
+
 	checkForMatch();
+
+	console.log(cardsInPlay);
+	console.log(suitsInPlay);
+	
+	this.setAttribute('src', finalImage);
 
 };
 
 var createBoard = function() {
+	var queensAvailable = 2;
+	var kingsAvailable = 2;
+
 	for(var i = 0; i < cards.length; i++){
+
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
@@ -63,10 +104,5 @@ var createBoard = function() {
 };
 
 
-var resetCards = function() {
-
-
-
-}
-
 createBoard();
+
