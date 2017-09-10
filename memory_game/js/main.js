c78193781fc81ec3e7682fafd04a353a84ec85e2
Raul Gutierrez <raulgutierrez@silver-owl.attlocal.net>
2017-09-10 13:45:22 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,31 +23,38 @@ var cards = [
 
 ];
 
+//Variables containing what is on the board
 var cardsInPlay = [];
 var suitsInPlay = [];
+
+//Variable containing win counter, initializes counter
 var wins = 0;
 document.getElementById('counter').textContent = wins;
 
-
+//Sets listener to thank donators
 var thanks = function() {
 	alert("Aww, you shouldn't have!");
 }
 document.getElementById('donate').addEventListener('click', thanks);
 
-
+//Function to flip card
 var flipCard = function(){
 	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
+	//Adds card attributes to arrays
 	cardsInPlay.push(cards[cardId].rank);
 	suitsInPlay.push(cards[cardId].suit);
 
+
 	this.setAttribute('src', cards[cardId].cardImage);
 
+	//Variable that will determine image at end of function
 	var finalImage = cards[cardId].cardImage;
 
+	//Reverts image to back face, removes card from arrays
 	var resetCard = function() {
 		finalImage = 'images/back.png';
 		cardsInPlay.pop();
@@ -58,6 +65,8 @@ var flipCard = function(){
 	var checkForMatch = function() {
 
 		if(cardsInPlay.length === 2) {
+
+			//Checks to see if card is clicked twice via suit, resets card
 			if(suitsInPlay[0] === suitsInPlay[1] && cardsInPlay[0] === cardsInPlay[1]){
 				cardsInPlay.pop();
 				suitsInPlay.pop();
@@ -65,6 +74,7 @@ var flipCard = function(){
 				return;
 			}
 
+			//For first pair found
 			if(cardsInPlay[0] === cardsInPlay[1]){
 				alert("You found a match!");
 				
@@ -75,7 +85,10 @@ var flipCard = function(){
 		
 		};
 
+		//For second pair found 
 		if(cardsInPlay.length === 4){
+
+			//Checks to see if card is clicked twice via suit, resets card
 			if(suitsInPlay[2] === suitsInPlay[3] && cardsInPlay[2] === cardsInPlay[3]){
 				cardsInPlay.pop();
 				suitsInPlay.pop();
@@ -85,6 +98,8 @@ var flipCard = function(){
 
 			if(cardsInPlay[2] === cardsInPlay[3]){
 				alert("You found a match!");
+
+				//Updates win counter
 				wins = wins + 1;
 				document.getElementById('counter').textContent = wins;
 				
@@ -98,19 +113,32 @@ var flipCard = function(){
 
 	checkForMatch();
 
+	//Verifies arrays contain cards on the board
 	console.log(cardsInPlay);
 	console.log(suitsInPlay);
-	console.log(wins)
-	
+	console.log(wins);
+
+	//Revers image to back if card pressed twice, otherwise keeps face
 	this.setAttribute('src', finalImage);
 
 };
 
 var createBoard = function() {
-	// var queensAvailable = 2;
-	// var kingsAvailable = 2;
+
+	//Counters for the cards left to choose from in 'for' loop
+	var queensAvailable = 2;
+	var kingsAvailable = 2;
+	var cardsAvailable = (queensAvailable + kingsAvailable) - 1;
 
 	for(var i = 0; i < cards.length; i++){
+
+		//Generates random number to choose card from 'cards' object
+		var cardChosen = Math.floor(Math.random()*(cardsAvailable));
+		if (cardChosen === 0 || cardChosen === 1) {
+			queensAvailable - 1;
+		}else{
+			kingsAvailable - 1;
+		};
 
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
@@ -124,6 +152,7 @@ var createBoard = function() {
 createBoard();
 
 var resetAllCards = function() {
+	//Deletes div content, clears arrays, creates another board
 	document.getElementById('game-board').innerHTML = '';
 	cardsInPlay = [];
 	suitsInPlay = [];
