cc2d0af2ff15ac60d4134353d6cb0054e080a169
Raul Gutierrez <raulgutierrez@silver-owl.attlocal.net>
2017-09-10 00:04:25 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -18,36 +18,55 @@ var cards = [
 	{
 		rank: "king",
 		suit: "diamonds",
-		cardImage: "images/king-of-diamonds"
+		cardImage: "images/king-of-diamonds.png"
 	}
 
 ];
 
 var cardsInPlay = [];
 
-var checkForMatch = function(){
+var checkForMatch = function() {
 
 		if(cardsInPlay.length === 2){
 			if(cardsInPlay[0] === cardsInPlay[1]){
 				alert("You found a match!");
+				
+				resetCards();
 			}else{
 				alert("Sorry, try again!");
 		}
 	};
 
-}
+};
 
-var flipCard = function(cardID){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	console.log(cardId);
 
-	console.log("User flipped " + cards[cardID].rank);
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
-
-	cardsInPlay.push(cards[cardID].rank);
-
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 
+};
+
+var createBoard = function() {
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+
+var resetCards = function() {
+
+
+
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
